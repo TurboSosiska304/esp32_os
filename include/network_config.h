51c3510877235020e670e8583ad864c5bd66266b
TurboSosiska304 <kaz6r4@gmail.com>
2026-07-19 00:50:19 +0300

    remove_wolssh_and_wolssl

--- include/network_config.h
+++ include/network_config.h
@@ -4,6 +4,3 @@
 #define NETWORK_WIFI_SSID "OpenWrt_2.4"
 #define NETWORK_WIFI_PASSWORD "password_2.4"
 
-/* Reserved for the future wolfSSH authentication callback. */
-#define NETWORK_SSH_USERNAME "admin"
-#define NETWORK_SSH_PASSWORD "password"
